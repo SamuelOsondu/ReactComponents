fd9adf02c372e5ecb39eef96df42098ebd9ccb84
Samuel Osondu <82204866+SamuelOsondu@users.noreply.github.com>
2024-12-11 16:45:38 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Food from "./food";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <Food />
+  </div>,
+  document.getElementById("root")
+);
